102d14d3bc01180eee1805bef0d9dcac2d909ff3
ABOOGY13 <doubleaa676@gmail.com>
2026-01-06 21:43:01 -0600

    Add Next.js Pages index placeholder

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div>
+      <h1>AOTA Marketplace</h1>
+      <p>Welcome to the AOTA Marketplace. Use the navigation to access Marketplace and Raffle pages.</p>
+    </div>
+  );
+}
