77ae41d9f7de9e87af4335a833dcebe01029f4d7
ABOOGY13 <doubleaa676@gmail.com>
2026-01-27 11:02:52 -0600

    Revert "Nextjs pages"

--- pages/_app.js
+++ pages/_app.js
@@ -1,19 +1,17 @@
 import Link from "next/link";
-import { Analytics } from '@vercel/analytics/next';
 import '../styles/globals.css';
 
 export default function App({ Component, pageProps }) {
   return (
     <div style={{ fontFamily: 'Arial, sans-serif' }}>
       <nav style={{ padding: 20, borderBottom: '1px solid #ccc' }}>
-        <Link href="/" style={{ marginRight: 20 }}>Home</Link>
-        <Link href="/marketplace" style={{ marginRight: 20 }}>Marketplace</Link>
-        <Link href="/raffle">Raffle</Link>
+        <Link href="/"> <a style={{ marginRight: 20 }}>Home</a></Link>
+        <Link href="/marketplace"> <a style={{ marginRight: 20 }}>Marketplace</a></Link>
+        <Link href="/raffle"> <a>Raffle</a></Link>
       </nav>
       <main style={{ padding: 20 }}>
         <Component {...pageProps} />
       </main>
-      <Analytics />
     </div>
   );
 }
